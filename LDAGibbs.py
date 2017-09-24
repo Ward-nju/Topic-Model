92756a92c544d8680470d809e2ebddd818106484
Ward-nju <linghf1123@163.com>
2017-09-24 20:08:13 +0800

    Add files via upload

--- LDAGibbs.py
+++ LDAGibbs.py
@@ -5,6 +5,7 @@ import numpy as np
 from scipy.special import gamma
 from collections import OrderedDict
 import pandas as pd
+from numba import jit
 
 class DataPreProcessing(object):
     def __init__(self):
@@ -39,88 +40,116 @@ class LDAModel(object):
     Latent Dirichlet Allocation
     implementation of `Latent Dirichlet Allocation` by David M.Blei, et al. (2003)
     """
-    def __init__(self,dpre,K,alpha=0.1,beta=0.01,max_iter=100,seed=1,converge_criteria=0.001):
+    def __init__(self,dpre,K,alpha=0.1,beta=0.01,max_iter=100,seed=1):
         #initial var
         self.dpre=dpre
         self.K=K
+        self.V=dpre.words_count
         self.alpha=alpha
         self.beta=beta
         self.max_iter=max_iter
         self.seed=seed
-        self.converge_criteria=converge_criteria
         
-        dt=np.zeros([self.dpre.docs_count,self.K],dtype=int)   #docs*topics
-        tw=np.zeros([self.K,self.dpre.words_count],dtype=int)  #topics*words
-        dtsum=dt.sum(axis=1)    
-        twsum=tw.sum(axis=1)
-        Z=np.array([[0 for y in range(len(self.dpre.docs[x]))] for x in range(self.dpre.docs_count)])    #topic assignment for each word for each doc
+        self.dt=np.zeros([self.dpre.docs_count,self.K],dtype=int)   #docs*topics
+        self.tw=np.zeros([self.K,self.V],dtype=int)  #topics*words
+        self.dtsum=self.dt.sum(axis=1)    
+        self.twsum=self.tw.sum(axis=1)
+        self.Z_assignment=np.array([[0 for y in range(len(self.dpre.docs[x]))] for x in range(self.dpre.docs_count)])    #topic assignment for each word for each doc
         
-        #initialization
+        #output var:
+        self.theta=np.array([[0.0 for y in range(self.K)] for x in range(self.dpre.docs_count)])
+        self.phi=np.array([[0.0 for y in range(self.V)] for x in range(self.K)])      
+
+    @jit    
+    def initializeModel(self):
         np.random.seed(self.seed)
         for m in range(self.dpre.docs_count):
             for n in range(len(self.dpre.docs[m])):   #n is word's index
                 k=np.random.randint(0,self.K) 
-                dt[m,k]+=1
-                dtsum[m]+=1
-                tw[k,self.dpre.docs[m][n]]+=1 #pay attention!
-                twsum[k]+=1
-                Z[m][n]=k
+                self.dt[m,k]+=1
+                self.dtsum[m]+=1
+                self.tw[k,self.dpre.docs[m][n]]+=1 
+                self.twsum[k]+=1
+                self.Z_assignment[m][n]=k
         
-        #output var:
-        self.theta=np.array([[0.0 for y in range(self.K)] for x in range(self.dpre.docs_count)])
-        self.phi=np.array([[0.0 for y in range(self.dpre.words_count)] for x in range(self.K)])      
-
-                  
+    @jit
+    def inferenceModel(self):   
         #Gibbs sampling over burn-in period and sampling period
-        converge=False
-        cur_iter=0
+        self.initializeModel()
         
-        while not converge and (cur_iter<=self.max_iter):
+        cur_iter=0
+        while cur_iter<=self.max_iter:
             for m in range(self.dpre.docs_count):
                 for n in range(len(self.dpre.docs[m])):   #n is word's index
-                    topic=Z[m][n]
-                    word=self.dpre.docs[m][n]
-                    
-                    dt[m,topic]-=1
-                    dtsum[m]-=1
-                    tw[topic,word]-=1
-                    twsum[topic]-=1
-
-                    p=(tw[:,word]+self.beta)/(twsum+self.dpre.words_count*self.beta)*(dt[m,:]+self.alpha)/(dtsum[m]+self.K*self.alpha)
-                    topic=np.random.multinomial(1,p/p.sum()).argmax()
-
-                    dt[m,topic]+=1
-                    dtsum[m]+=1
-                    tw[topic,word]+=1
-                    twsum[topic]+=1
-                    Z[m][n]=topic                        
+                    self.sample(m,n)
             #print(cur_iter)
             cur_iter+=1
-            
-        #output
-        for m in range(self.dpre.docs_count):
-            self.theta[m]=(dt[m]+self.alpha)/(dtsum[m]+self.alpha*self.K)
-        for k in range(self.K):
-            self.phi[k]=(tw[k]+self.beta)/(twsum[k]+self.beta*self.dpre.words_count)
+        
+        self.updateEstimatedParameters()
+ 
+    @jit
+    def sample(self,m,n):
+        topic=self.Z_assignment[m][n]
+        word=self.dpre.docs[m][n]
+                    
+        self.dt[m,topic]-=1
+        self.dtsum[m]-=1
+        self.tw[topic,word]-=1
+        self.twsum[topic]-=1
 
-    def print_topics(self,topN=10):
+        p=(self.tw[:,word]+self.beta)/(self.twsum+self.V*self.beta)\
+           *(self.dt[m,:]+self.alpha)/(self.dtsum[m]+self.K*self.alpha)
+           
+        topic=np.random.multinomial(1,p/p.sum()).argmax()
+
+        self.dt[m,topic]+=1
+        self.dtsum[m]+=1
+        self.tw[topic,word]+=1
+        self.twsum[topic]+=1
+        self.Z_assignment[m][n]=topic 
+           
+        
+    @jit
+    def updateEstimatedParameters(self):
+        for m in range(self.dpre.docs_count):
+            self.theta[m]=(self.dt[m]+self.alpha)/(self.dtsum[m]+self.alpha*self.K)
         for k in range(self.K):
-            s=''
+            self.phi[k]=(self.tw[k]+self.beta)/(self.twsum[k]+self.beta*self.V)
+
+    def print_tw(self,topN=10):
+        topics=[]
+        for k in range(self.K):
+            topic=[]
             index=self.phi[k].argsort()[::-1][:topN]
             for ix in index:
                 prob=("%.3f"%self.phi[k,ix])
                 word=self.dpre.id2word[ix]
-                s+=str(prob)+'*'+word+' + '
-            print('topic'+str(k)+':  '+s)
-   
-                
-if __name__=='__main__':
-    corpus=[['apple','orange','banana'],
-            ['apple','orange'],
+                topic.append((prob,word))
+            topics.append(topic)
+        return topics
+    
+    def print_dt(self):
+        return self.theta
+            
+def test():
+    corpus=[['apple','orange','banana'],            
             ['banana','orange'],
             ['cat','dog'],
             ['dog','tiger'],
-            ['tiger','cat']]
+            ['tiger','cat'],
+            ['computer','AI','network'],
+            ['computer','IR','algorithm'],
+            ['computer','IR','algorithm','network'],
+            ['apple','orange'],
+            ['algorithm','process','ML'],
+            ['dog','tiger']]
     dpre=preprocessing(corpus)
-    K=2
-    model=LDAModel(dpre,K)      
+    
+    K=3
+    model=LDAModel(dpre,K)
+    model.inferenceModel()  
+    
+    
+if __name__=='__main__':
+     test()
+
